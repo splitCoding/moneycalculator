7f2545469b84c2f63259a91fe6498de23f7ecee2
splitube <voicesplit@daum.net>
2021-05-17 16:20:13 +0900

    let month => monthStart / borderradius

--- fullCalendar.js
+++ fullCalendar.js
@@ -6,40 +6,45 @@ const thirtyOneArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
 const nextMonthBtns = document.querySelector("#nextMonth");
 const previousMonthBtns = document.querySelector("#previousMonth");
 const year = document.querySelector("#year");
-let setYear = 2021;
+const today = new Date();
+let setYear = today.getFullYear();
 
-function makeCalendar(start){
-  if(-24 < start && start <= 0){
-    if(start > -12){
-      start += 12;
+function makeCalendar(startMonth){
+
+  //2019에서 2021년도까지 표현할 수 있게 그 이상은 alert
+  if(-24 < startMonth && startMonth <= 0){
+    if(startMonth > -12){
+      startMonth += 12;
       setYear = 2020;
-    } else if(start <= -12){
-      start +=24;
+    } else if(startMonth <= -12){
+      startMonth +=24;
       setYear = 2019;
     }
-  } else if(start <= -24){
+  } else if(startMonth <= -24){
     alert('2019년도까지만 존재합니다');
     return;
   }
-  if(start>12){
+  if(startMonth>12){
     alert('2021년도까지만 존재합니다');
     return;
   }
+
+
+  function putMonth(){
+    const monthArea = document.querySelector('#month');
+    monthArea.textContent = `${startMonth}월`;
+  }
+
   let date = new Date();
-  let month = start;
-  date.setFullYear(`${setYear}`,`${month-1}`,1);
+  date.setFullYear(`${setYear}`,`${startMonth}`,1);
   year.textContent = `${setYear}년`;
   let firstDay = date.getDay();
 
-  function changeMonth(){
-    const monthArea = document.querySelector('#month');
-    monthArea.textContent = `${month}월`;
-  }
   
   let isItThirty = false;
 
   function thiryDaytoggle(){
-    if(thirtyMonth.includes(month)){
+    if(thirtyMonth.includes(startMonth)){
       isItThirty = true;
       console.log(isItThirty);
     } else {
@@ -60,13 +65,13 @@ function makeCalendar(start){
     }
   }
 
-  changeMonth();
   thiryDaytoggle();
+  putMonth();
   fillDays();
 }
 
 function reset(){
-  if(start < -23 || start > 12){
+  if(startMonth < -23 || startMonth > 12){
     return;
   }
   for(let index of calendarIndex){
@@ -74,8 +79,8 @@ function reset(){
   }
 }
 
-let start= 5;
-makeCalendar(start);
+let startMonth= today.getMonth()+1;
+makeCalendar(startMonth);
 
-nextMonthBtns.addEventListener('click',()=>{start++; reset(); makeCalendar(start)});
-previousMonthBtns.addEventListener('click',()=>{start--; reset(); makeCalendar(start)})
+nextMonthBtns.addEventListener('click',()=>{startMonth++; reset(); makeCalendar(startMonth)});
+previousMonthBtns.addEventListener('click',()=>{startMonth--; reset(); makeCalendar(startMonth)})
