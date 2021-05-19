6320479207bcbf3619149bc3d6f4b8d77f5fbd64
splitube <voicesplit@daum.net>
2021-05-19 16:18:00 +0900

    원하는 년도와 월의 달력을 표시하는 기능 추가

--- makeFullCalendar.js
+++ makeFullCalendar.js
@@ -4,10 +4,23 @@ const monthArea = document.querySelector('#month');
 const yearArea = document.querySelector("#year");
 const nextMonthBtns = document.querySelector("#nextMonth");
 const previousMonthBtns = document.querySelector("#previousMonth");
+const inputFullDate = document.querySelector('#inputFullDate');
+const inputs = inputFullDate.querySelectorAll('input');
+const inputBtn = inputFullDate.querySelector('button');
+const inputYear = inputs[0];
+const inputMonth = inputs[1];
 
 const thirtyMonth = [4,6,9,11];
 const thirtyArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 const thirtyOneArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
+//모든 날짜칸을 비우는 함수
+function reset(){
+  for(let index of calendarIndex){
+    index.textContent="";
+  }
+}
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 //오늘날짜기준 month 구하는 함수
 function updateMonth(){
@@ -31,7 +44,6 @@ function whatIsFirstDay(){
 //달력을 만드는 함수
 let addMonth = 0;
 let addYear = 0;
-// addYear += Math.floor((Math.abs(addMonth)-1)/12);
 
 function makeCalendar(){
   reset();
@@ -47,7 +59,7 @@ function makeCalendar(){
     startYear += -1+parseInt((startMonth+1)/12);
     startMonth = -12*(parseInt((startMonth+1)/12)-1) - Math.abs(startMonth);
   }
-  
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
   //startYear을 year로 startMonth를 month로 설정하고 화면에 표시하는 함수실행
 
   function setYearAndMonth(){
@@ -109,13 +121,90 @@ function makeCalendar(){
 }
 makeCalendar();
 
-//모든 날짜칸을 비우는 함수
-function reset(){
-  for(let index of calendarIndex){
-    index.textContent="";
+let addinputMonth = 0;
+let addinputYear = 0;
+
+//Input으로 바로 날짜를 바꿀때 사용하는 함수
+function makeCalendarWhenInputExist(){
+  reset();
+  yearArea.textContent = "";
+  monthArea.textContent = "";
+  let today2 = new Date();
+  let startYear = Number(inputYear.value) + addinputYear;
+  let startMonth = Number(inputMonth.value) + addinputMonth -1;
+  console.log(startYear,startMonth);
+    
+  //현재month를 기준으로 이전과 이후로 이동할때 startMonth, startYear 재지정
+  if(startMonth >= 0){
+    startYear+=parseInt(startMonth/12)
+    startMonth%=12
+  } else if(startMonth < 0){
+    startYear += -1+parseInt((startMonth+1)/12);
+    startMonth = -12*(parseInt((startMonth+1)/12)-1) - Math.abs(startMonth);
   }
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  //startYear을 year로 startMonth를 month로 설정하고 화면에 표시하는 함수실행
+
+  function setYearAndMonth(){
+    today2.setFullYear(`${startYear}`,`${startMonth}`,1);
+    yearArea.textContent = `${startYear}년`;
+    monthArea.textContent = `${startMonth+1}월`;
+  }
+  setYearAndMonth();
+
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  //30일로 된 month인지 알아내는 함수실행
+
+  let isItThirty = false;
+
+  function thirtyDaytoggler(){
+    if(thirtyMonth.includes(startMonth%12+1)){
+      isItThirty = true;
+    } else {
+      isItThirty = false;
+    }
+  }
+  thirtyDaytoggler();
+
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  //day를 표시하는 함수실행(2월은 28일 윤달은 29일)
+  
+  function fillDays(){
+    let day = today2.getDay();
+    if(isItThirty===true){
+      for(let i =0;i<thirtyArray.length;i++){
+        calendarIndex[day+i].textContent = thirtyArray[i];
+        calendarIndex[day+i].classList.add('dayActive');
+      }
+    } else if(isItThirty===false){
+      if((startMonth)%12 === 1){
+        if(startYear%4===0){
+          for(let i =0;i<thirtyOneArray.length-2;i++){
+            calendarIndex[day+i].textContent = thirtyOneArray[i];
+            calendarIndex[day+i].classList.add('dayActive');
+          }
+        } else { 
+          for(let i =0;i<thirtyOneArray.length-3;i++){
+            calendarIndex[day+i].textContent = thirtyOneArray[i];
+            calendarIndex[day+i].classList.add('dayActive');
+          }
+        }
+      } else {
+        if(isItThirty===false){
+          for(let i =0;i<thirtyOneArray.length;i++){
+            calendarIndex[day+i].textContent = thirtyOneArray[i];
+            calendarIndex[day+i].classList.add('dayActive');
+          }
+        }
+      }
+    }
+  }
+  fillDays(); 
+  //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  
 }
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 
 nextMonthBtns.addEventListener('click',()=>{addMonth++; makeCalendar()});
 previousMonthBtns.addEventListener('click',()=>{addMonth--; makeCalendar()});
+inputBtn.addEventListener('click',makeCalendarWhenInputExist);
