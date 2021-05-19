b13e45505b0417d0c38022e8c6b4f802e12fddbc
splitube <voicesplit@daum.net>
2021-05-19 21:35:37 +0900

    makeFullCalendar.js 파일 재검토및 수정, 날짜가 없는 칸은 pooUp안나옴

--- js/makeFullCalendar.js
+++ js/makeFullCalendar.js
@@ -16,39 +16,47 @@ const thirtyMonth = [4,6,9,11];
 const thirtyArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 const thirtyOneArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
 
-//모든 날짜칸을 비우는 함수
+//모든 년도, 월, 날짜칸을 비우고 dayActive클래스 삭제 함수
 function reset(){
   for(let index of calendarIndex){
     index.textContent="";
+    yearArea.textContent = "";
+    monthArea.textContent = "";
+    if(Array.from(index.classList).includes('dayActive')){
+      index.classList.remove("dayActive");
+      index.removeEventListener('click',showPopUp);
+    }
   }
 }
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
-//오늘날짜기준 month 구하는 함수
-function updateMonth(){
-  let date = new Date();
-  date.setMonth(date2.getMonth());
-}
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//popUp창이 나타나게 하는 함수
+function showPopUp(){ popUp.style.display = "inline-block"; }
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
-//오늘을 기준으로 이번달 1일이 무슨 요일인지 구하는 함수(setFullYear로 지정했기떄문에 사용하지 않음)
-function whatIsFirstDay(){
-  let firstDay = today.getDay();
-  
-  if(firstDay < 0){
-    return firstDay += 7;
-  } else {
-    return firstDay;
+//dayActive클래스가 있는 칸을 클릭시 popUp창 나타나는 함수
+function dayActivePopUp(array){
+  for(let element of array){
+    if(Array.from(element.classList).includes('dayActive')){
+      element.addEventListener('click',showPopUp);
+    }
   }
 }
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
-//달력을 만드는 함수
+//다음달, 이전달, 입력한 년도와 달로 이동하는 버튼 기능
+nextMonthBtns.addEventListener('click',()=>{addMonth++; makeCalendar()});
+previousMonthBtns.addEventListener('click',()=>{addMonth--; makeCalendar()});
+inputBtn.addEventListener('click',makeCalendarWhenInputExist);
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
 let addMonth = 0;
 let addYear = 0;
 
+//달력을 만드는 함수*********************************************
 function makeCalendar(){
   reset();
+
   let today = new Date();
   let startMonth = today.getMonth()+addMonth;
   let startYear = today.getFullYear()+addYear;
@@ -62,20 +70,18 @@ function makeCalendar(){
     startMonth = -12*(parseInt((startMonth+1)/12)-1) - Math.abs(startMonth);
   }
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-  //startYear을 year로 startMonth를 month로 설정하고 화면에 표시하는 함수실행
 
+  //startYear을 year로 startMonth를 month로 설정하고 화면에 표시하는 함수 실행
   function setYearAndMonth(){
     today.setFullYear(`${startYear}`,`${startMonth}`,1);
     yearArea.textContent = `${startYear}년`;
     monthArea.textContent = `${startMonth+1}월`;
   }
   setYearAndMonth();
-
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
   //30일로 된 month인지 알아내는 함수실행
-
   let isItThirty = false;
-
   function thirtyDaytoggler(){
     if(thirtyMonth.includes(startMonth%12+1)){
       isItThirty = true;
@@ -84,10 +90,9 @@ function makeCalendar(){
     }
   }
   thirtyDaytoggler();
-
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
   //day를 표시하는 함수실행(2월은 28일 윤달은 29일)
-  
   function fillDays(){
     let day = today.getDay();
     if(isItThirty===true){
@@ -118,33 +123,31 @@ function makeCalendar(){
       }
     }
   }
-
   fillDays(); 
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-  const dayActive = document.querySelectorAll(".dayActive");
-  dayActivePopUp(dayActive);
+
+  //dayActivePopUp함수를 dayActive 클래스를 가진 모든요소에 대입
+  let dayActiveAfter = document.querySelectorAll(".dayActive");
+  dayActivePopUp(dayActiveAfter);
 }
-makeCalendar();
+//***************************************************** 
+
 
 let addinputMonth = 0;
 let addinputYear = 0;
 
-//Input으로 바로 날짜를 바꿀때 사용하는 함수
+//Input으로 날짜를 바꿨을때 달력만들어주는 함수********************************
 function makeCalendarWhenInputExist(){
+  //숫자가 아닌 값이 입력됐을때 return;
   if((inputYear.value-'0')!==Number(inputYear.value)||(inputMonth.value-'0')!==Number(inputMonth.value)){
     alert("숫자를 입력해주세요");
     return;
   }
-  
+
   reset();
-  yearArea.textContent = "";
-  monthArea.textContent = "";
   let today2 = new Date();
   let startYear = (inputYear.value-'0') + addinputYear;
   let startMonth = (inputMonth.value-'0') + addinputMonth -1;
-
-  
-  console.log(startYear,startMonth);
     
   //현재month를 기준으로 이전과 이후로 이동할때 startMonth, startYear 재지정
   if(startMonth >= 0){
@@ -155,18 +158,17 @@ function makeCalendarWhenInputExist(){
     startMonth = -12*(parseInt((startMonth+1)/12)-1) - Math.abs(startMonth);
   }
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-  //startYear을 year로 startMonth를 month로 설정하고 화면에 표시하는 함수실행
 
+  //startYear을 year로 startMonth를 month로 설정하고 화면에 표시하는 함수실행
   function setYearAndMonth(){
     today2.setFullYear(`${startYear}`,`${startMonth}`,1);
     yearArea.textContent = `${startYear}년`;
     monthArea.textContent = `${startMonth+1}월`;
   }
   setYearAndMonth();
-
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-  //30일로 된 month인지 알아내는 함수실행
 
+  //30일로 된 month인지 알아내는 함수실행
   let isItThirty = false;
 
   function thirtyDaytoggler(){
@@ -177,10 +179,9 @@ function makeCalendarWhenInputExist(){
     }
   }
   thirtyDaytoggler();
-
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
   //day를 표시하는 함수실행(2월은 28일 윤달은 29일)
-  
   function fillDays(){
     let day = today2.getDay();
     if(isItThirty===true){
@@ -213,18 +214,14 @@ function makeCalendarWhenInputExist(){
   }
   fillDays(); 
   //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+  //다음달, 이전달 버튼비활성화
   nextMonthBtns.style.display = "none";
   previousMonthBtns.style.display ="none";
-  const dayActive = document.querySelectorAll(".dayActive");
-  dayActivePopUp(dayActive);
+  
+  let dayActiveAfter = document.querySelectorAll(".dayActive");
+  dayActivePopUp(dayActiveAfter);
 }
-
-
-//다음달, 이전달, 입력한 년도와 달로 이동하는 버튼 기능
-nextMonthBtns.addEventListener('click',()=>{addMonth++; makeCalendar()});
-previousMonthBtns.addEventListener('click',()=>{addMonth--; makeCalendar()});
-inputBtn.addEventListener('click',makeCalendarWhenInputExist);
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//***************************************************************
 
 //현재 기준의 년도와 달로 다시 이동하는 버튼 기능
 backToNowBtn.addEventListener('click',()=>{
@@ -236,21 +233,11 @@ backToNowBtn.addEventListener('click',()=>{
 });
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
-//날짜가 입력된 칸을 작성시 popUp창 나타나는 함수
-function dayActivePopUp(calendarIndex){
-  Array.from(calendarIndex).forEach((each)=>{
-    each.addEventListener('click',()=>{
-      document.querySelector("#popUp").style.display = "inline-block";
-      })
-    }
-  );
-}
-
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-
 //popUp창의 x버튼을 누를시 popUp창 사라짐
 const popUpBtn = document.querySelector("#popUp img");
 popUpBtn.addEventListener('click',()=>{
   document.querySelector("#popUp").style.display = "none";
 })
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
+makeCalendar();
